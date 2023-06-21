a9f1d84b164c026c9578df1f5c5cbec5a3b3567e
Giorgiana Coman <giorgiana.coman@theaccessgroup.com>
2023-06-21 09:09:05 +0300

    Add project files.

---
+++ Passcode/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var password = "";
+            while (password != "secret")
+            {
+                Console.WriteLine("Your password : ");
+                password = Console.ReadLine();
+
+                if (password != "secret")
+
+                    Console.WriteLine("Wrong password");
+            }
+            Console.WriteLine("You are authenticate");
+        }
+
+
+    }
+}
