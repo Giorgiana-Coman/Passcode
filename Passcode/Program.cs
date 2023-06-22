f49b710930bef655821b145babcb2f728a8e8c27
Giorgiana Coman <giorgiana.coman@theaccessgroup.com>
2023-06-22 11:09:43 +0300

    change 3

--- Passcode/Program.cs
+++ Passcode/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management.Instrumentation;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,9 @@ namespace Passcode
                 //On Branch1 create at least 1 commit and push those changes 
 
                 //change 2
+
+
+                //commit 3
 
                 if (password != "secret")
 
